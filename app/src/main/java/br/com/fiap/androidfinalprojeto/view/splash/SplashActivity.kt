d5494ffd76d89aa1722e6a2a9a439c18674dbe18
Renato Sousa <renato.sousa@supplier.com.br>
2019-10-23 15:56:35 -0300

    Código da Splash Screen. 3,5 Segundos.

--- app/src/main/java/br/com/fiap/androidfinalprojeto/view/splash/SplashActivity.kt
+++ app/src/main/java/br/com/fiap/androidfinalprojeto/view/splash/SplashActivity.kt
@@ -1,13 +1,27 @@
 package br.com.fiap.androidfinalprojeto.view.splash
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import br.com.fiap.androidfinalprojeto.R
+import br.com.fiap.androidfinalprojeto.view.main.MainActivity
 
 class SplashActivity : AppCompatActivity() {
+
+    private val timeSplash = 3500L
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        load()
+    }
+
+    private fun load() {
+        Handler().postDelayed({
+            val startProgram = Intent(this@SplashActivity, MainActivity::class.java)
+            startActivity(startProgram)
+            finish()
+        }, timeSplash)
     }
 }
