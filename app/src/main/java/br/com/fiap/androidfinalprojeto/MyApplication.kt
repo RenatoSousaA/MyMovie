6353418a1937082d97ff37f0328f24a416716301
Renato Sousa <renato.sousa@supplier.com.br>
2019-11-19 11:01:20 -0300

    Remotes Configs

--- app/src/main/java/br/com/fiap/androidfinalprojeto/MyApplication.kt
+++ app/src/main/java/br/com/fiap/androidfinalprojeto/MyApplication.kt
@@ -28,5 +28,6 @@ class MyApplication : Application() {
                 dbModule
             ))
         }
+
     }
 }
--- app/src/main/java/br/com/fiap/androidfinalprojeto/view/main/MainActivity.kt
+++ app/src/main/java/br/com/fiap/androidfinalprojeto/view/main/MainActivity.kt
@@ -2,6 +2,8 @@ package br.com.fiap.androidfinalprojeto.view.main
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
@@ -12,7 +14,12 @@ import com.google.android.material.navigation.NavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import br.com.fiap.androidfinalprojeto.R
+import com.google.firebase.database.BuildConfig
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings
 import kotlinx.android.synthetic.main.all_movies_recyclerview.*
+import kotlinx.android.synthetic.main.nav_header_main.*
+import kotlinx.android.synthetic.main.nav_header_main.view.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -39,6 +46,35 @@ class MainActivity : AppCompatActivity() {
                 R.id.nav_all_movies, R.id.nav_about, R.id.nav_sair
             ), drawerLayout
         )
+
+        val remoteConfig = FirebaseRemoteConfig.getInstance()
+
+        val configSettings = FirebaseRemoteConfigSettings.Builder()
+            .setMinimumFetchIntervalInSeconds(10)
+            .build()
+
+        remoteConfig.setConfigSettingsAsync(configSettings)
+
+        val subtitleNavigation = remoteConfig.getString("subtitle_navigation")
+        val titleNavigation = remoteConfig.getString("title_navigation")
+
+        remoteConfig.setDefaultsAsync(R.xml.remote_config_defauts)
+
+        remoteConfig.fetchAndActivate()
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val headerView = navView.getHeaderView(0)
+                    val navTitle = headerView.findViewById(R.id.title_navigationView) as TextView
+                    val navSubTitle = headerView.findViewById(R.id.subtitle_navigationView) as TextView
+                    navTitle.text = titleNavigation
+                    navSubTitle.text = subtitleNavigation
+                } else {
+                    Toast.makeText(this, "Fetch failed",
+                        Toast.LENGTH_SHORT).show()
+                }
+            }
+
+
 
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
